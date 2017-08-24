90416cf28ea51603c2d396b407f104fe9a1b6cac
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-24 11:48:20 +0200

    FZ54A-60/searching-products/ copying part of HomeView to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,23 +3,27 @@ import React from 'react'
 import {Navbar, Header, Brand, FomrGroup, FormGroup, FormControl, Button} from 'react-bootstrap'
 
 const SearchBar = (
-    <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">Brand</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Navbar.Form pullLeft>
-                <FormGroup>
-                    <FormControl type="text" placeholder="Search" />
-                </FormGroup>
-                {' '}
-                <Button type="submit">Submit</Button>
-            </Navbar.Form>
-        </Navbar.Collapse>
-    </Navbar>
+    <div>
+        <Navbar>
+            <div className="navbar--contaiter">
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a className="logo"><img src={logo} alt="logo" height="50"/></a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Form pullLeft>
+                        <FormGroup>
+                            <FormControl className="search-input" type="text" placeholder="Znajdź produkt"/>
+                        </FormGroup>
+                        {' '}
+                        <Button className="search-button" type="submit"><img src={magnifier} alt="search" height="35"/></Button>
+                    </Navbar.Form>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    </div>
 );
 
 export default SearchBar
