fcba5c7c2e7e904173875eab3df431d0876caa55
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-24 03:55:29 +0200

    FZ54A-60/searching-products/ adding styles to search bar

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -7,21 +7,23 @@ import magnifier from "../../img/magnifier.png"
 const HomeView = () => (
     <div>
         <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <a className="logo"><img src={logo} alt="logo" height="40"/></a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Navbar.Form pullLeft>
-                    <FormGroup>
-                        <FormControl className="search-input" type="text" placeholder="Znajdź produkt"/>
-                    </FormGroup>
-                    {' '}
-                    <Button type="submit"><img src={magnifier} alt="search" height="20"/></Button>
-                </Navbar.Form>
-            </Navbar.Collapse>
+            <div className="navbar-contaiter1">
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a className="logo"><img src={logo} alt="logo" height="50"/></a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Form pullLeft>
+                        <FormGroup>
+                            <FormControl className="search-input" type="text" placeholder="Znajdź produkt"/>
+                        </FormGroup>
+                        {' '}
+                        <Button type="submit"><img src={magnifier} alt="search" height="35"/></Button>
+                    </Navbar.Form>
+                </Navbar.Collapse>
+            </div>
         </Navbar>
     </div>
 )
