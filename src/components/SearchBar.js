69d68656bc30e434cfba42ccb0f9b048be94cb9c
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-27 10:45:31 +0200

    FZ54A-60-searching-products/ extracting SearchBar to separate component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,32 @@
 import React from 'react'
 
-import {Navbar, Header, Brand, FomrGroup, FormGroup, FormControl, Button} from 'react-bootstrap'
+import {Navbar, FormGroup, FormControl, Button} from 'react-bootstrap'
+import logo from "../img/logo.svg"
+import magnifier from "../img/magnifier.png"
+import '../components/SearchBar.css'
 
-const SearchBar = (
-    <div>
-        <Navbar>
-            <div className="navbar--contaiter">
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a className="logo"><img src={logo} alt="logo" height="50"/></a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Form pullLeft>
-                        <FormGroup>
-                            <FormControl className="search-input" type="text" placeholder="Znajdź produkt"/>
-                        </FormGroup>
-                        {' '}
-                        <Button className="search-button" type="submit"><img src={magnifier} alt="search" height="35"/></Button>
-                    </Navbar.Form>
-                </Navbar.Collapse>
-            </div>
-        </Navbar>
-    </div>
+const SearchBar = () => (
+  <div>
+    <Navbar>
+      <div className="navbar--contaiter">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a className="logo"><img src={logo} alt="logo" height="50"/></a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Znajdź produkt"/>
+            </FormGroup>
+            {' '}
+            <Button className="search-button" type="submit"><img src={magnifier} alt="search" height="35"/></Button>
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  </div>
 );
 
 export default SearchBar
--- src/components/SearchResultsList.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Piotrek on 25.08.2017.
- */
