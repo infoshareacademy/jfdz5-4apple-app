1793bb1ddb3dd1ef289ad8eab77a8eb3624cdd24
dceynowa <dejw.ce@gmail.com>
2017-08-27 19:11:23 +0200

    FZ54A-58 add register form view

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import SearchBar from '../SearchBar'
+import RegistrationForm from "./RegistrationForm";
 
 const HomeView = () => (
   <div>
     <SearchBar/>
+      <RegistrationForm/>
   </div>
 )
 
---
+++ src/components/views/RegistrationForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class RegistrationForm extends React.Component {
+    render() {
+        return (
+            <div>
+                RegistationForm
+            </div>
+        )
+    }
+}
+
+export default RegistrationForm
