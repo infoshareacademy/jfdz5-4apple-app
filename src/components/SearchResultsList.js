c86ea923c0f1207bed41e271e38540a80c9fea36
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-27 10:18:13 +0200

    FZ54A-62-search-results-list/ extracting SerachResultsList to separate component

--- src/components/SearchResultsList.js
+++ src/components/SearchResultsList.js
@@ -1,16 +1,42 @@
 import React from 'react'
 
+import {ListGroupItem, ListGroup, Button} from "react-bootstrap";
 import searchResults from '../components/_utils/searchingProducts'
-import {ListGroupItem, ListGroup} from "react-bootstrap";
 
-const ProductsList = (
+const SearchResultsList = () => (
+  <div className="container products--container">
     <ListGroup>{
-        searchResults.map(function (product) {
-            return <ListGroupItem>
-                {product.brand}
-            </ListGroupItem>
-        })
+      searchResults.map(function (product) {
+        return <ListGroupItem >
+          <div className="product--container">
+            <img className="product--img" src={product.image}/>
+            <div className="product--info">
+              <div className="product--description">
+                <div className="product--name">
+                  <h2>{product.brand + " " + product.model}</h2>
+                </div>
+                <div className="product--details">
+                  {product.description}
+                  <div>
+                    <h4>Kolor: {product.color}</h4>
+                    <h4>Rozmiar: {product.size}</h4>
+                  </div>
+                </div>
+              </div>
+              <div className="product--price">
+                <h3 className="price">od: <span className="price--currency"><span
+                  className="price">{(product.price).toFixed(2)}</span> zł</span></h3>
+                <Button bsSize="large" bsStyle="primary"
+                        className="button--continue">Porównaj</Button>
+                <h6>w {"*quantity*"} sklepach</h6>
+              </div>
+            </div>
+          </div>
+        </ListGroupItem >
+      })
     }
     </ListGroup>
+  </div>
 );
-export default ProductsList
+
+export default SearchResultsList
--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,50 +1,14 @@
 import React from 'react'
 
-import {Button} from 'react-bootstrap'
-import ProductsList from "../SearchResultsList";
-import searchResults from '../_utils/searchingProducts'
-import {ListGroupItem, ListGroup} from "react-bootstrap";
+import SearchResultsList from "../SearchResultsList";
 import SearchBar from '../SearchBar'
 
 const HomeView = () => (
-    <div>
-        <SearchBar/>
-        <div className="container products--container">
-            <ListGroup>{
-                searchResults.map(function (product) {
-                    return <ListGroupItem >
-                        <div className="product--container">
-                            <img className="product--img" src={product.image}/>
-                            <div className="product--info">
-                                <div className="product--description">
-                                    <div className="product--name">
-                                        <h2>{product.brand + " " + product.model}</h2>
-                                    </div>
-                                    <div className="product--details">
-                                        {product.description}
-                                        <div>
-                                            <h4>Kolor: {product.color}</h4>
-                                            <h4>Rozmiar: {product.size}</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="product--price">
-                                    <h3 className="price">od: <span className="price--currency"><span
-                                        className="price">{(product.price).toFixed(2)}</span> zł</span></h3>
-                                    <Button bsSize="large" bsStyle="primary"
-                                            className="button--continue">Porównaj</Button>
-                                    <h6>w {"*quantity*"} sklepach</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </ListGroupItem >
-                })
-            }
-            </ListGroup>
-        </div>
-    </div>
+  <div>
+    <SearchBar/>
+    <SearchResultsList/>
+  </div>
 )
-
 
 export default HomeView
 
