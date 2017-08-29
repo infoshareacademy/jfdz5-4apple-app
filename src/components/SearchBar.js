517376b55e07944fcb881426e1e7e2bcab4db9ad
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-29 05:50:22 +0200

    FZ54A-60-searching-products/ fixing RWD after adding form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,28 +5,42 @@ import logo from "../img/logo.svg"
 import magnifier from "../img/magnifier.png"
 import '../components/SearchBar.css'
 
-const SearchBar = () => (
-  <div>
-    <Navbar>
-      <div className="navbar--contaiter">
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a className="logo"><img src={logo} alt="logo" height="50"/></a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Navbar.Form pullLeft>
-            <FormGroup>
-              <FormControl type="text" placeholder="Znajdź produkt"/>
-            </FormGroup>
-            {' '}
-            <Button className="search-button" type="submit"><img src={magnifier} alt="search" height="35"/></Button>
-          </Navbar.Form>
-        </Navbar.Collapse>
+class SearchBar extends React.Component {
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(this.target.value);
+  //
+  // };
+
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <div className="navbar--contaiter">
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a className="logo"><img src={logo} alt="logo" height="50"/></a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Navbar.Form pullLeft>
+                <form className="search--form" onSubmit={this.handleSubmit}>
+                  <FormGroup >
+                    <FormControl type="text" placeholder="Znajdź produkt"/>
+                  </FormGroup>
+                  {' '}
+                  <Button className="search-button" type="submit"><img src={magnifier} alt="search"
+                                                                       height="35"/></Button>
+                </form>
+              </Navbar.Form >
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
       </div>
-    </Navbar>
-  </div>
-);
+    )
+  }
+}
 
 export default SearchBar
