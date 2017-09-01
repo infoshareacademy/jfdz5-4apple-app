191827018d97446a84db22800e1d902aa045e04a
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-01 08:17:11 +0200

    FZ54A-61-products-filters/ creating files structure

---
+++ src/components/SearchBarWithFilters.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SearchBar from './SearchBar'
+
+class ProductsFilters extends React.Component {
+
+  render() {
+    return (<div>
+      <SearchBar/>
+      <h1>Filtry</h1>
+    </div>)
+  }
+}
+
+export default ProductsFilters
