92e57e28d062c399e386bb0500356ed70bd18129
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-02 13:25:22 +0200

    FZ54A-61-products-filters/ adding more fiter templates

--- src/components/SearchBarWithFilters.js
+++ src/components/SearchBarWithFilters.js
@@ -1,30 +1,42 @@
 import React from 'react'
 
 import SearchBar from './SearchBar'
-import {Checkbox, ControlLabel, FormControl, FormGroup, Panel, PanelGroup} from "react-bootstrap";
+import {Checkbox, ControlLabel, FormControl, FormGroup, Panel, PanelGroup, FieldGroup} from "react-bootstrap";
 
 class ProductsFilters extends React.Component {
 
+
   render() {
+    // console.log(products)
+
     return (<div>
       <SearchBar/>
+
+
       <PanelGroup defaultActiveKey="1" accordion>
         <Panel header="Sprecyzuj wyszukiwanie" eventKey="1">
+          <form>
+            <ControlLabel >Cena
+              <FormControl type="text"/>
+              -
+              <FormControl type="text"/>
+            </ControlLabel>
+            <Checkbox >
+              Damskie
+            </Checkbox>
+            <Checkbox >
+              Męskie
+            </Checkbox>
 
-          <Checkbox checked readOnly>
-            Checkbox
-          </Checkbox>
-          <Checkbox checked readOnly>
-            Checkbox
-          </Checkbox>
-
-          <FormGroup controlId="formControlsSelect">
-            <ControlLabel>Select</ControlLabel>
-            <FormControl componentClass="select" placeholder="select">
-              <option value="select">select</option>
-              <option value="other">...</option>
-            </FormControl>
-          </FormGroup>
+            <FormGroup controlId="formControlsSelect">
+              <ControlLabel>Wybierz rozmiar</ControlLabel>
+              <FormControl componentClass="select" placeholder="select">
+                {}
+                <option value="select">select</option>
+                <option value="other">...</option>
+              </FormControl>
+            </FormGroup>
+          </form>
         </Panel>
       </PanelGroup>
 
