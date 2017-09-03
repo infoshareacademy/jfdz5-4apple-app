f71829cfe88a7b80c6c4633555fe9ef85b086a97
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-03 19:04:36 +0200

    FZ54A-60-searching-products-new/ adding SearchBar to ProductDetailsView

--- src/components/views/ProductDetailsView.js
+++ src/components/views/ProductDetailsView.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import DataFetcher from "../DataFetcher/DataFetcher";
+import SearchBar from "../SearchBar";
 
 const ProductDetailsView = () => (
   <div>
+    <DataFetcher
+      dataUrl={'http://localhost:3000/data/products.json'}
+      component={SearchBar}
+      propName="searchedProducts"
+    />
     <h1>Product Details</h1>
   </div>
 )
