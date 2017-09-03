0eca43953ff59dd7b9529bf4e901d9b078e4bcc6
Pawel Sajenko <bolsik@gmail.com>
2017-09-03 20:55:08 +0200

    building reducers

--- src/components/views/SignIn.js
+++ src/components/views/SignIn.js
@@ -27,30 +27,29 @@ handleLoginChange = (event) => {
                 <Form horizontal>
                     <FormGroup controlId="formHorizontalEmail">
                         <Col componentClass={ControlLabel} sm={2}>
-                            Login
                         </Col>
-                        <Col sm={2}>
+                        <Col smOffset={3} sm={2}>
                             <FormControl onChange={ this.handleLoginChange } type="text" placeholder="login" />
                         </Col>
                     </FormGroup>
 
                     <FormGroup controlId="formHorizontalPassword">
                         <Col componentClass={ControlLabel} sm={2}>
-                            Hasło
+
                         </Col>
-                        <Col sm={2}>
+                        <Col smOffset={3} sm={2}>
                             <FormControl onChange={ this.handlePasswordChange} type="password" placeholder="hasło" />
                         </Col>
                     </FormGroup>
 
                     <FormGroup>
-                        <Col smOffset={2} sm={10}>
+                        <Col smOffset={6} sm={10}>
                             <Checkbox>Zapamiętaj mnie</Checkbox>
                         </Col>
                     </FormGroup>
 
                     <FormGroup>
-                        <Col smOffset={2} sm={10}>
+                        <Col smOffset={6} sm={10}>
                             <Button bsStyle="primary" type="submit">
                                 Zaloguj
                             </Button>
@@ -62,5 +61,9 @@ handleLoginChange = (event) => {
         )
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    check: (userName, userEmail, password) => dispatch(check(userName,userEmail,password))
+});
 
 export default SignIn
