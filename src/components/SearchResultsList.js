ed5b46a070b8d580f5b7a77b46ed0d1d65d9be6a
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-03 04:52:51 +0200

    FZ54A-60-searching-products-new/ exporting list items as separate component

--- src/components/SearchResultsList.js
+++ src/components/SearchResultsList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import {ListGroupItem, ListGroup} from "react-bootstrap";
-import {Link} from 'react-router-dom'
+import {ListGroup} from "react-bootstrap";
 import '../components/SearchResultsList.css'
 import {connect} from "react-redux";
+
 import {search} from "../state/searching";
 import NoItemFound from "./NoItemFound";
-import ButtonBlue from "./Button";
+import SearchResultsListItem from "./SearchResultsListItem";
 
 class SearchResultsList extends React.Component {
 
@@ -49,34 +49,7 @@ class SearchResultsList extends React.Component {
           searchResults.toString() === "" ?
             <NoItemFound/>
             :
-            searchResults.map(function (product, index) {
-              return <ListGroupItem key={index}>
-                <div className="product--container">
-                  <img className="product--img" alt="product" src={product.image}/>
-                  <div className="product--info">
-                    <div className="product--description">
-                      <div className="product--name">
-                        <h2>{product.brand} {product.model} {product.author} {product.title} </h2>
-                      </div>
-                      <div className="product--details">
-                        {product.description}
-                        <div>
-                          <h4>{product.color ? ("Kolor: " + product.color) : null} </h4>
-                          <h4>{product.size ? ("Rozmiar: " + product.size) : null} {}</h4>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="product--price">
-                      <h3 className="price">od: <span className="price--currency"><span
-                        className="price">{(product.price).toFixed(2)}</span> zł</span></h3>
-                      <Link to={`/results/details/${index}`}><ButtonBlue textContent={"Porównaj"}/>
-                      </Link>
-                      <h6>w {product.shops.length} sklepach</h6>
-                    </div>
-                  </div>
-                </div>
-              </ListGroupItem >
-            })
+            <SearchResultsListItem searchResults={searchResults}/>
         }
         </ListGroup>
       </div>
