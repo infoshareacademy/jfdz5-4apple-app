707d2b38ef1c2d30aa73579820bb352037a51859
dceynowa <dejw.ce@gmail.com>
2017-09-21 11:51:04 +0200

    change const constructor on class constructot in tabbed results - it doesn't works

--- src/components/TabbedResults.js
+++ src/components/TabbedResults.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {connect} from "react-redux";
 
 import {
     Tab,
@@ -10,19 +11,46 @@ import SearchResultsList from './SearchResultsList'
 import SearchResultsGrid from './SearchResultsGrid'
 import {filterResults} from "../state/searching";
 
-const TabbedResults = ({products}) => (
-    <div>
-        <SearchBar filteredResults={filterResults}/>
-        <Tabs defaultActiveKey={1} id="tabbed-results">
-            <Tab eventKey={1} title="List">
-                <SearchResultsList products={products}/>
-            </Tab>
+class TabbedResults extends React.Component {
+    render() {
+        const searchResults = this.props.filteredResults
 
-            <Tab eventKey={2} title="Grid">
-                <SearchResultsGrid products={products}/>
-            </Tab>
-        </Tabs>
-    </div>
-)
+        return (
+            <div>
+                <SearchBar searchResults={searchResults}/>
+                <Tabs defaultActiveKey={1} id="tabbed-results">
+                    <Tab eventKey={1} title="List">
+                        <SearchResultsList products={this.props.products}/>
+                    </Tab>
 
-export default TabbedResults
+                    <Tab eventKey={2} title="Grid">
+                        <SearchResultsGrid products={this.props.products}/>
+                    </Tab>
+                </Tabs>
+            </div>
+        )
+    }
+}
+// const TabbedResults = ({products}) => (
+//     <div>
+//         <SearchBar filteredResults={filterResults}/>
+//         <Tabs defaultActiveKey={1} id="tabbed-results">
+//             <Tab eventKey={1} title="List">
+//                 <SearchResultsList products={products}/>
+//             </Tab>
+//
+//             <Tab eventKey={2} title="Grid">
+//                 <SearchResultsGrid products={products}/>
+//             </Tab>
+//         </Tabs>
+//     </div>
+// )
+
+export default connect(
+    state => ({
+        filteredResults: state.searching.filteredResults
+    }),
+    dispatch => ({
+        addSearchedResults: (searchedProducts, searchedItem) => dispatch(filterResults(searchedProducts, searchedItem))
+    })
+)(TabbedResults)
