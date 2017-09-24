6649b5e19b9bc543eaf359909789b39bb06455ba
dceynowa <dejw.ce@gmail.com>
2017-09-24 20:51:23 +0200

    style results grid view

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {Switch, Route,} from 'react-router-dom'
 
 import HomeView from './views/HomeView'
-import SearchResultsListView from './views/SearchResultsListView'
-import SearchResultsGridView from './views/SearchResultsGridView'
 import ProductDetailsView from './views/ProductDetailsView'
 import TabbedResults from './TabbedResults'
 
@@ -11,7 +9,7 @@ const Content = () => (
     <div>
         <Switch>
             <Route exact path="/" component={HomeView}/>
-            <Route exact path="/results/" component={TabbedResults}/>
+            <Route exact path="/results" component={TabbedResults}/>
             <Route path="/results/details/:productId" component={ProductDetailsView}/>
             <Route render={() => <h1>Not found</h1>}/>
         </Switch>
