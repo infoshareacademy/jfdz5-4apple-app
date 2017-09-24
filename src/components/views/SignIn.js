58eab94b8e007f5d0de199214e83e1bd862359b3
sajen87 <bolsik@gmail.com>
2017-09-24 14:29:12 +0200

    not working SM login

--- src/components/views/SignIn.js
+++ src/components/views/SignIn.js
@@ -1,46 +1,78 @@
 import React from 'react'
 import {Button, Col, ControlLabel, Form, FormControl, FormGroup, Checkbox} from 'react-bootstrap'
 import {connect} from 'react-redux'
-import { set } from '../../state/auth'
+import {set} from '../../state/auth'
 import firebase from 'firebase'
 
-const providerForFacebook = new firebase.auth.FacebookAuthProvider()
-const providerForGoogle = new firebase.auth.GoogleAuthProvider()
+const providerForGoogle = new firebase.auth.GoogleAuthProvider();
+const providerForFacebook = new firebase.auth.FacebookAuthProvider();
+
+providerForFacebook.addScope('user_birthday');
 
 class SignIn extends React.Component {
 
     state = {
         login: '',
         password: ''
-    }
+    };
 
     handleLoginChange = (event) => {
         this.setState({
             login: event.currentTarget.value
         })
-    }
+    };
 
     handlePasswordChange = (event) => {
         this.setState({
             password: event.currentTarget.value
         })
-    }
+    };
 
     handleSubmit = (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
         firebase.auth().signInWithEmailAndPassword(this.state.login, this.state.password).then(
-              user => {
-                this.props.setUser(user)
+            user => {
+                this.props.setUser(user);
                 firebase.database().ref('favorites/' + user.uid).on('value', snapshot => {
                     console.log(snapshot.val())
-                })
-                firebase.database().ref('favorites/' + user.uid).set({ zupa: 9000000 })
+                });
+                firebase.database().ref('favorites/' + user.uid).set({email: 1})
             }
         ).catch(
             err => console.log(err)
         )
-    }
+    };
+
+    handlerGoogleLogIn = (event) => {
+        event.preventDefault();
+        firebase.auth().signInWithPopup(providerForGoogle).then(user => {
+                // this.props.setUser(user)
+                firebase.database().ref('favorites/' + user.uid).on('value', snapshot => {
+                    console.log(snapshot.val())
+                });
+                firebase.database().ref('favorites/' + user.uid).set({})
+            }
+        ).catch(
+            err => console.log(err)
+        )
+    };
+
+    handlerFacebookLogIn = (event) => {
+        event.preventDefault();
+        firebase.auth().signInWithPopup(providerForFacebook).then(user => {
+               // const token = result.credential.accessToken;
+               //  const user = result.user;
+                firebase.database().ref('favorites/' + user.uid).on('value', snapshot => {
+                    console.log(snapshot.val())
+                });
+                firebase.database().ref('favorites/' + user.uid).set({})
+            }
+        ).catch(
+            err => console.log(err)
+        )
+    };
+
 
     render() {
         return (
@@ -76,6 +108,19 @@ class SignIn extends React.Component {
                             </Button>
                         </Col>
                     </FormGroup>
+
+                    <FormGroup>
+                        <Col smOffset={4} sm={10}>
+                            <Button onClick={this.handlerGoogleLogIn} bsStyle="primary" type="submit">
+                                google
+                            </Button>
+                        </Col>
+                        <Col smOffset={4} sm={10}>
+                            <Button onClick={this.handlerFacebookLogIn} bsStyle="primary" type="submit">
+                                FB
+                            </Button>
+                        </Col>
+                    </FormGroup>
                 </Form>
             </div>
         )
@@ -86,4 +131,4 @@ const mapDispatchToProps = dispatch => ({
     setUser: user => dispatch(set(user))
 });
 
-export default connect(null, ma pDispatchToProps)(SignIn)
+export default connect(null, mapDispatchToProps)(SignIn)
