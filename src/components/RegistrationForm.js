3fbbc1c0bcc9a4fede95f09172b5bfb2751d77d0
dceynowa <dejw.ce@gmail.com>
2017-09-27 01:42:37 +0200

    FZ54A-58 simple styling registration form

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import firebase from 'firebase'
-import {Button, Col, Form, FormControl, FormGroup, Alert} from "react-bootstrap";
+import {Button,  Form, FormControl, FormGroup, Alert} from "react-bootstrap";
 
 import logo from '../img/logo.svg'
 
@@ -45,45 +45,38 @@ class RegistrationForm extends React.Component {
     render() {
         return (
             <div className="container">
+                <img src={logo} alt="logo" className="registation-form__logo"/>
+
                 <div className="registation-form__content">
-                    <img src={logo} alt="logo" className="registation-form__logo"/>
-                    <h2>Zarejestruj się</h2>
                     <Form horizontal onSubmit={this.handleSubmit} className="registation-form__form">
-
-
+                        <h2>Zarejestruj się</h2>
                         <FormGroup controlId="formHorizontalEmail" onChange={this.handleChange}>
 
-                            <Col sm={12}>
                                 <FormControl type="email"
                                              placeholder="Email"
                                              name="email"
                                              value={this.state.email}
                                              required/>
-                            </Col>
                         </FormGroup>
 
                         <FormGroup controlId="formHorizontalPassword"
                                    onChange={this.handleChange}>
-                            <Col sm={12}>
                                 <FormControl
                                     type="password"
                                     placeholder="Podaj hasło"
                                     name="password"
                                     value={this.state.password}
                                     required/>
-                            </Col>
                         </FormGroup>
 
                         <FormGroup controlId="formHorizontalPassword"
                                    onChange={this.handleChange}>
-                            <Col sm={12}>
                                 <FormControl
                                     type="password"
                                     placeholder="Powtórz hasło"
                                     name="confirmPassword"
                                     value={this.state.confirmPassword}
                                     required/>
-                            </Col>
                         </FormGroup>
                         {
                             this.state.error === null ? null : <Alert bsStyle="danger">{this.state.error}</Alert>
@@ -91,11 +84,9 @@ class RegistrationForm extends React.Component {
                         }
 
                         <FormGroup>
-                            <Col sm={12}>
                                 <Button type="submit">
                                     Zarejestruj się
                                 </Button>
-                            </Col>
                         </FormGroup>
 
                     </Form>
