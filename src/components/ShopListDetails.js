c441d31afc40d2e7f5b3670d73432482044fb345
Aleksander Zych <Konfucjussz29@gmail.com>
2017-09-26 23:28:18 +0200

    find item id

---
+++ src/components/ShopListDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class ShopListDetails extends React.Component{
+
+render(){
+  return  <h1>{this.props.item}</h1>
+
+}
+
+}
+export default ShopListDetails
--- src/components/views/ProductDetailsView.js
+++ src/components/views/ProductDetailsView.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import DataFetcher from "../DataFetcher/DataFetcher";
 import SearchBar from "../SearchBar";
+import ShopListDetails from "../ShopListDetails";
 
-const ProductDetailsView = () => (
-  <div>
-    <DataFetcher
-      dataUrl={'http://localhost:3000/data/products.json'}
-      component={SearchBar}
-      propName="searchedProducts"
-    />
-    <h1>Product Details</h1>
-  </div>
-)
+//console.log(this.props.params.productId)
+const ProductDetailsView = (props) => {
+    const productId = props.match.params.productId;
+    console.log(productId)
+return (
+    <div>
+        <DataFetcher
+            dataUrl={'http://localhost:3000/data/products.json'}
+            component={SearchBar}
+            propName="searchedProducts"
+        />
+        <ShopListDetails item={productId}/>
+    </div>)
+
+}
+
 
 
 export default ProductDetailsView
