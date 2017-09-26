2189580889e084281049d1c6f2932a537f2b7639
Aleksander Zych <Konfucjussz29@gmail.com>
2017-09-27 00:19:26 +0200

    import json

--- src/components/ShopListDetails.js
+++ src/components/ShopListDetails.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import products from '../../data/products.json'
 
 class ShopListDetails extends React.Component{
 
 render(){
+    const x = products;
   return  <h1>{this.props.item}</h1>
 
 }
