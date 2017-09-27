3d160a5d279a274d59ceffc2df07b3b99ec53735
dceynowa <dejw.ce@gmail.com>
2017-09-27 20:15:45 +0200

    FZ54A-58 show alert message when passwords are different

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import firebase from 'firebase'
-import {Button,  Form, FormControl, FormGroup, Alert} from "react-bootstrap";
+import {Button, Form, FormControl, FormGroup, Alert} from "react-bootstrap";
 
 import logo from '../img/logo.svg'
 
@@ -23,23 +23,22 @@ class RegistrationForm extends React.Component {
 
 
     handleSubmit = (event) => {
+        event.preventDefault();
 
         if (this.state.password === this.state.confirmPassword) {
-            event.preventDefault();
             firebase.auth().createUserWithEmailAndPassword(
                 this.state.email,
                 this.state.password
             )
         } else {
-            event.preventDefault();
-            err => this.setState({error: err.message})
-            console.log('Podane hasła różnią się od siebie')
             this.setState({
                 email: this.state.email,
                 password: '',
-                confirmPassword: ''
+                confirmPassword: '',
+                error: 'Podane hasła różnią się od siebie'
             })
         }
+
     };
 
     render() {
@@ -52,31 +51,31 @@ class RegistrationForm extends React.Component {
                         <h2>Zarejestruj się</h2>
                         <FormGroup controlId="formHorizontalEmail" onChange={this.handleChange}>
 
-                                <FormControl type="email"
-                                             placeholder="Email"
-                                             name="email"
-                                             value={this.state.email}
-                                             required/>
+                            <FormControl type="email"
+                                         placeholder="Email"
+                                         name="email"
+                                         value={this.state.email}
+                                         required/>
                         </FormGroup>
 
                         <FormGroup controlId="formHorizontalPassword"
                                    onChange={this.handleChange}>
-                                <FormControl
-                                    type="password"
-                                    placeholder="Podaj hasło"
-                                    name="password"
-                                    value={this.state.password}
-                                    required/>
+                            <FormControl
+                                type="password"
+                                placeholder="Podaj hasło"
+                                name="password"
+                                value={this.state.password}
+                                required/>
                         </FormGroup>
 
                         <FormGroup controlId="formHorizontalPassword"
                                    onChange={this.handleChange}>
-                                <FormControl
-                                    type="password"
-                                    placeholder="Powtórz hasło"
-                                    name="confirmPassword"
-                                    value={this.state.confirmPassword}
-                                    required/>
+                            <FormControl
+                                type="password"
+                                placeholder="Powtórz hasło"
+                                name="confirmPassword"
+                                value={this.state.confirmPassword}
+                                required/>
                         </FormGroup>
                         {
                             this.state.error === null ? null : <Alert bsStyle="danger">{this.state.error}</Alert>
@@ -84,9 +83,9 @@ class RegistrationForm extends React.Component {
                         }
 
                         <FormGroup>
-                                <Button type="submit">
-                                    Zarejestruj się
-                                </Button>
+                            <Button type="submit">
+                                Zarejestruj się
+                            </Button>
                         </FormGroup>
 
                     </Form>
