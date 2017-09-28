f279265bd20ef3692b6615efabc72ae5274e64a7
dceynowa <dejw.ce@gmail.com>
2017-09-28 23:21:47 +0200

    FZ54A-65 add chosen products to favs

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,40 +1,46 @@
 import React from 'react'
-import {ListGroupItem} from "react-bootstrap";
-import {Link} from 'react-router-dom'
-import ButtonBlue from "./ButtonBlue"
-
+import {ListGroupItem, Button} from "react-bootstrap";
+import firebase from 'firebase'
 
 const ShopList = (props) => {
 
-    const {shops} = props;
-    console.log(shops);
-    return (
-        <div>
-            {shops.map((product, index) => {
-                return (
-                    <ListGroupItem key={index}>
-                        <div className="product--container">
-                            <img className="shop--img" alt="product" src={product.image}/>
-                            <div className="product--info">
-                                <div className="product--description">
-                                    <div className="product--name">
-                                        <h2>{product.name}{product.author} {product.title} </h2>
-                                    </div>
-                                </div>
-                                <div className="product--price">
-                                    <h3 className="price"><span className="price--currency"><span
-                                        className="price">{(product.localPrice).toFixed(2)}</span> zł</span></h3>
-                                    <Link to={`/results/details/orders/${index}`}><ButtonBlue textContent={"Dodaj do listy życzeń"}/>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </ListGroupItem>
-                )
+  const {shops} = props;
+  console.log(shops);
+  return (
+    <div>
+      {shops.map((product, index) => {
+        return (
+          <ListGroupItem key={index}>
+            <div className="product--container">
+              <img className="shop--img" alt="product" src={product.image}/>
+              <div className="product--info">
+                <div className="product--description">
+                  <div className="product--name">
+                    <h2>{product.name}{product.author} {product.title} </h2>
+                  </div>
+                </div>
+                <div className="product--price">
+                  <h3 className="price"><span className="price--currency"><span
+                    className="price">{(product.localPrice).toFixed(2)}</span> zł</span></h3>
+                  <Button bsSize="large"
+                          bsStyle="primary"
+                          className="button--continue"
+                          onClick={
+                            () => firebase.database().ref(
+                              '/listOfFavsThings/' + firebase.auth().currentUser.uid + '/'
+                              + product.name + ':' + product.productId)
+                              .set(true)
+                          }>Dodaj do listy życzeń </Button>
 
-            })}
-        </div>
-    )
+                </div>
+              </div>
+            </div>
+          </ListGroupItem>
+        )
+
+      })}
+    </div>
+  )
 }
 
 
