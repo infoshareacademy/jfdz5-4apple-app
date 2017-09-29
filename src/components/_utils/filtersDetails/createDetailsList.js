8d21ee6637e29f6ace68b2a2a3d9ea8efcb0a6bc
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-29 21:11:50 +0200

    FZ54A-61-products-filters/ fixing listing filter function

--- src/components/_utils/filtersDetails/createDetailsList.js
+++ src/components/_utils/filtersDetails/createDetailsList.js
@@ -1,9 +1,11 @@
 const createDetailsList = (results, filterName) => {
-  return results.reduce((productFirst, productNext) => {
-    return productNext[filterName].concat(productFirst[filterName])
-  }).filter((value, index, inputArray) => {
+  return results.map((product) => {
+    return product[filterName]
+  }).reduce((productFirst, productNext) => {
+    return productFirst.concat(productNext)
+  }, []).filter((value, index, inputArray) => {
     return inputArray.indexOf(value) === index
-  }).sort()
+  })
 };
 //array.indexOf finds 1st occurrence of argument passed. If certain value is doubled, next occurrence index will be different then indexOf(argument). That's why filter returns only one of any values to array.
 
