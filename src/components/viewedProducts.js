642ad7c335a508f9e6e22929b447ddeefa24215d
Aleksander Zych <Konfucjussz29@gmail.com>
2017-10-01 12:53:53 +0200

    viewed_products/ add button

--- src/components/viewedProducts.js
+++ src/components/viewedProducts.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import Button from "react-bootstrap/es/Button";
 
 
-class viewedProducts extends React.Component{
+class viewedProducts extends React.Component {
 
 
-    render(){
+    render() {
 
+        const userId = firebase.auth().currentUser.uid;
+        return (
 
-
-        return <div>xXx</div>
-
+            <Button bsSize="large"
+                    bsStyle="primary"
+                    className="button--continue"
+                    onClick={
+                        () => firebase.database().ref(
+                            '/viewed/' + userId
+                            + '/' + "buty" + ':' + "blabla"
+                        )
+                            .set(true)
+                    }>Dodaj do listy życzeń </Button>
+        )
     }
 }
+
 export default viewedProducts
