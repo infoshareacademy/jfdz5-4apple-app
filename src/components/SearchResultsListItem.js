b94225f748516428ef9fdd302aa871d81b47faa5
Aleksander Zych <konfucjusz29@gmail.com>
2017-10-01 16:28:30 +0200

    add handle viewed

--- src/components/SearchResultsListItem.js
+++ src/components/SearchResultsListItem.js
@@ -11,66 +11,76 @@ import './SearchResultsList.css'
 const SearchResultsListItem = ({searchResults, allProducts}) => {
 
 
-  return (
-    <div>
-      {searchResults.map((product, index) => {
-        return (
-          <ListGroupItem key={index}>
-            {/*<Button onClick={handleSaveFavseClick}/>*/}
-            <div className="product--container">
-              <img className="product--img" alt="product" src={product.image}/>
-              <div className="product--info">
-                <div className="product--description">
-                  <div className="product--name">
-                    <h2>{product.brand} {product.model} {product.author} {product.title} </h2>
-                  </div>
-                  <div className="product--details">
-                    {product.description}
-                    <div>
-                      <h4>{product.color ? ("Kolor: " + product.color) : null} </h4>
-                      <h4>{product.size ? ("Rozmiar: " + product.size) : null} {}</h4>
-                      <h4>{product.pages ? ("Liczba stron: " + product.pages) : null} </h4>
-                      <h4>{product.cover ? ("Okładka: " + product.cover) : null} {}</h4>
-                    </div>
-                  </div>
-                </div>
-                <div className="product--price">
-                  <h3 className="price">od: <span className="price--currency"><span
-                    className="price">{(product.price).toFixed(2)}</span> zł</span></h3>
-                  <Button bsSize="large"
-                          bsStyle="primary"
-                          className="button--continue"
+    return (
+        <div>
+            {searchResults.map((product, index) => {
+                return (
+                    <ListGroupItem key={index}>
+                        {/*<Button onClick={handleSaveFavseClick}/>*/}
+                        <div className="product--container">
+                            <img className="product--img" alt="product" src={product.image}/>
+                            <div className="product--info">
+                                <div className="product--description">
+                                    <div className="product--name">
+                                        <h2>{product.brand} {product.model} {product.author} {product.title} </h2>
+                                    </div>
+                                    <div className="product--details">
+                                        {product.description}
+                                        <div>
+                                            <h4>{product.color ? ("Kolor: " + product.color) : null} </h4>
+                                            <h4>{product.size ? ("Rozmiar: " + product.size) : null} {}</h4>
+                                            <h4>{product.pages ? ("Liczba stron: " + product.pages) : null} </h4>
+                                            <h4>{product.cover ? ("Okładka: " + product.cover) : null} {}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="product--price">
+                                    <h3 className="price">od: <span className="price--currency"><span
+                                        className="price">{(product.price).toFixed(2)}</span> zł</span></h3>
+                                    <Button bsSize="large"
+                                            bsStyle="primary"
+                                            className="button--continue"
+                                            onClick={
+                                                () => {
+                                                    let productWithBrand = `${product.model}`;
+                                                    const userId = firebase.auth().currentUser.uid;
+                                                    console.log(allProducts)
 
-                          onClick={
-                            () => {
-                              let productWithBrand = `${product.brand} ${product.model}`;
-                              let productWithAuthor = `${product.author} ${product.title}`;
+                                                    firebase.database().ref(
+                                                        '/FavsProducts/' + userId + '/' + product.id
+                                                    )
+                                                        .set(productWithBrand)
+                                                }}> <i className="fa fa-star-o"/>
+                                    </Button>
+                                    <Link to={`/results/details/${product.id}`}><a   onClick={
+                                        () => {
+                                            let productWithBrand = `${product.model}`;
+                                            let productWithAuthor = `${product.title}`;
+                                            const userId = firebase.auth().currentUser.uid;
+                                            console.log(allProducts)
 
-                              console.log(allProducts)
-                              const userId = firebase.auth().currentUser.uid;
-                              firebase.database().ref(
-                                '/FavsProducts/' + userId + '/' + product.id
-                              )
-                                .set(productWithBrand || productWithAuthor)
-                            }}> <i className="fa fa-star-o"/>
-                  </Button>
-                  <Link to={`/results/details/${product.id}`}><ButtonBlue textContent={"Porównaj"}/>
-                  </Link>
 
-                  <h6>w {product.shops.length} sklepach</h6>
-                </div>
-              </div>
-            </div>
-          </ListGroupItem>
-        )
+                                            firebase.database().ref(
+                                                '/viewed/' + userId + '/' + product.id
+                                            )
+                                                .set(productWithBrand || productWithAuthor)
+                                        }}><ButtonBlue textContent={"Porównaj"}/></a>
+                                    </Link>
 
-      })} )</div>
-  )
+                                    <h6>w {product.shops.length} sklepach</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </ListGroupItem>
+                )
+
+            })} )</div>
+    )
 }
 
 export default connect(
-  state => ({
-    allProducts: state.allProducts.data
-  }),
-  dispatch => ({})
+    state => ({
+        allProducts: state.allProducts.data
+    }),
+    dispatch => ({})
 )(SearchResultsListItem)
