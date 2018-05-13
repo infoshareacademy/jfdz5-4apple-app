cdde2b24286e6e5fea72f5c5abc82083715f9398
Piotr Stefanowicz <stefan8357@gmail.com>
2018-05-13 18:08:58 +0200

    Adding external links buttons to shops

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import { ListGroupItem } from "react-bootstrap";
+import ButtonBlue from "./ButtonBlue";
+import { Link } from "react-router-dom";
 
 const ShopList = (props) => {
 
-  const {shops} = props;
-
-
   return (
     <div>
-
-      {shops.map((product, index) => {
+      {props.shops.map((product, index) => {
         return (
           <ListGroupItem key={index}>
-            <div className="product__container">
+            <div className="product__container shops">
               <img className="shop__img" alt="product" src={product.image}/>
-              <div className="product__info">
                 <div className="product__description">
                   <div className="product--name">
                     <h2>{product.name}{product.author} {product.title} </h2>
@@ -23,9 +20,12 @@ const ShopList = (props) => {
                 <div className="product__price">
                   <h3 className="price"><span className="price--currency"><span
                     className="price">{(product.localPrice).toFixed(2)}</span> zł</span></h3>
+                  <Link to={product.url} target='_blank'>
+                    <ButtonBlue helperClass={'button--small'}
+                                textContent={"Przejdź do sklepu"}/>
+                  </Link>
                 </div>
               </div>
-            </div>
           </ListGroupItem>
         )
       })}
